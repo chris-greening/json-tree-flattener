0be6bbb627fb72076fdeda972bad1a725f6a0f48
chris-greening <chrisgreening3@gmail.com>
2020-10-19 21:11:30 -0400

    Rewrote to implement with FlatJSONDict

--- test_flattener.py
+++ test_flattener.py
@@ -1,7 +1,7 @@
 import json
 import pprint
 
-from json_flattener import JsonFlattener
+from flat_json import JSONDict
 
 with open('test.json', 'r') as injson:
     json_str = injson.read()[3:]
@@ -11,5 +11,5 @@ print("WITHOUT FLATTENING " + "-"*20)
 pprint.pprint(json_dict)
 
 print("\n\n\n\n\n\nWITH FLATTENING " + "-"*20)
-flat_json = JsonFlattener(json_dict)
-pprint.pprint(flat_json.flat_json)
+flat_json = JSONDict(json_dict)
+pprint.pprint(flat_json)
