66db7efc99e03e0ca50085e3cd0dbbd80bbc9919
chris-greening <chrisgreening3@gmail.com>
2020-10-20 14:26:09 -0400

    refactor: Removed unnecessary for loop

--- python3/json_flattener.py
+++ python3/json_flattener.py
@@ -15,20 +15,23 @@ class JsonFlattener:
 
         self.json_tree = JsonTree(self.json_dict)
 
-        self.flat_json = self.flatten_json()
+        self.flat_json = self._flatten_json()
 
-    def flatten_json(self):
+    def _flatten_json(self):
+        """
+        Primary algorithm that creates the flattened dictionary from a mapped
+        tree of JSON data
+        """
         flattened_dict = {}
         for leaf_node in self.json_tree.leaf_nodes:
             key_arr = deque([])
-            for key in leaf_node.json_data.keys():
-                key_arr.appendleft(str(key))  # ensure key is str
-                new_key = '_'.join(key_arr)
-                if new_key in flattened_dict:
-                    for prior_key in leaf_node.prior_keys[-2::-1]:
-                        key_arr.appendleft(str(prior_key))
-                        new_key = '_'.join(key_arr)
-                        if new_key not in flattened_dict:
-                            break
-                flattened_dict[new_key] = list(leaf_node.json_data.values())[0]
+            for key in leaf_node.prior_keys[::-1]:
+                new_key = self._new_key(key, key_arr)
+                if new_key not in flattened_dict:
+                    break
+            flattened_dict[new_key] = list(leaf_node.json_data.values())[0]
         return flattened_dict
+
+    def _new_key(self, key: str, key_arr: deque) -> str:
+        key_arr.appendleft(str(key))
+        return '_'.join(key_arr)
