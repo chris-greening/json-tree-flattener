7f22f466776e629c58d482e47c4e9a98872ee80c
chris-greening <chrisgreening3@gmail.com>
2020-10-20 15:15:59 -0400

    Renamed mapTree -> _mapTree to indicate private

--- javascript/tree.js
+++ javascript/tree.js
@@ -3,10 +3,11 @@ let Node = require('./node.js')
 class Tree {
     constructor(jsonDict) {
         this.jsonDict = jsonDict;
-        this.mapTree(this.jsonDict);
+        this._mapTree(this.jsonDict);
     }
 
-    mapTree(jsonDict) {
+    _mapTree(jsonDict) {
+        //Get edges to all Node's inside the Tree
         this.leafNodes = [];
         this.rootNode = Node(jsonData, tree=this);
     }
