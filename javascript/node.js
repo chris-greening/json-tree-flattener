6ecb0c50c03377ba0d158cd83189706c626cc67e
chris-greening <chrisgreening3@gmail.com>
2020-10-20 14:57:45 -0400

    Added isLeaf getter and _determineObjectType private method

--- javascript/node.js
+++ javascript/node.js
@@ -1,7 +1,30 @@
 class Node {
-    constructor(jsonData) {
+    constructor(jsonData, tree, linkedList=[], priorKeys=[]) {
         this.jsonData = jsonData;
+        this.tree = tree;
+        this.linkedList = linkedList;
+        this.priorKeys = this.priorKeys;
+
+        this.dtype = _determineObjectType();
+
     }
+
+    _determineObjectType() {
+        //determine if the object passed is an array
+        if (typeof (this.jsonData) == 'object') {
+            if (this.jsonData.constructor == Object) {
+                this.dtype = 'object';
+            } else {
+                this.dtype = 'array';
+            }
+        }
+    }
+
+    get isLeaf() {
+        //Determine whether the node is a leaf node
+        return ((this.dtype != 'object') && (this.dtype != 'array'));
+    }
+
 }
 
 module.exports = Node;
