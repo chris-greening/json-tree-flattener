4adeeb034452d35e4172f0ab8d0263d863a6bf7a
chris-greening <chrisgreening3@gmail.com>
2020-10-20 22:30:35 -0400

    Printed final output

--- javascript/json_tree_flattener.js
+++ javascript/json_tree_flattener.js
@@ -4,8 +4,15 @@ let Tree = require('./tree.js')
 class JsonTreeFlattener {
     constructor(jsonDict){
         this.jsonDict = jsonDict;
+
+        this.jsonTree = new Tree(this.jsonDict);
     }
 }
 
 const test = new JsonTreeFlattener(jsonDict);
 
+// console.log(test.jsonTree.leafNodes);
+
+for (i=0; i<test.jsonTree.leafNodes.length; i++) {
+    console.log(test.jsonTree.leafNodes[i].jsonData);
+}
