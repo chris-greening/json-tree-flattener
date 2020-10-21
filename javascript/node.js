aaf2bd7d824c2110100931c341463debc664a6bf
chris-greening <chrisgreening3@gmail.com>
2020-10-20 22:31:17 -0400

    Added array and object recursion for getting edges

--- javascript/node.js
+++ javascript/node.js
@@ -3,28 +3,35 @@ class Node {
         this.jsonData = jsonData;
         this.tree = tree;
         this.linkedList = linkedList;
-        this.priorKeys = this.priorKeys;
+        this.priorKeys = priorKeys;
 
-        this.dtype = _determineObjectType();
-
+        this.dtype = this._determineObjectType(this.jsonData);
+        // console.log(this.dtype);
+        this.nodes = []
+        
         if (this.isLeaf) {
-
+            var lastKey = this.priorKeys[this.priorKeys.length-1];
+            var lastValue = this.jsonData;
+            this.jsonData = {};
+            this.jsonData[lastKey] = lastValue;
+            this.tree.leafNodes.push(this);
         } else {
             this._getEdges()
         }
-
-
     }
 
-    _determineObjectType() {
+    _determineObjectType(jsonData) {
         //determine if the object passed is an array
-        if (typeof (this.jsonData) == 'object') {
-            if (this.jsonData.constructor == Object) {
-                this.dtype = 'object';
+        if (typeof(jsonData) == "object") {
+            if (Object.prototype.toString.call(jsonData) == "[object Object]") {
+                var dtype = 'object';
             } else {
-                this.dtype = 'array';
+                var dtype = 'array';
             }
+        } else {
+            dtype = typeof(jsonData);
         }
+        return dtype;
     }
 
     get isLeaf() {
@@ -34,7 +41,33 @@ class Node {
 
     _getEdges() {
         //Get edges to other nodes in tree recursively
-        // var iterArr = ;
+        if (this.dtype == 'array') {
+            for (i = 0; i < this.jsonData.length; i++) {
+                var currentData = this.jsonData[i];
+
+                //Construct references for the next Node
+                var nextLinkedList = [...this.linkedList];
+                var nextPriorKeys = [...this.priorKeys];
+                nextLinkedList.push(this);
+                nextPriorKeys.push(i);
+
+                var newNode = new Node(currentData, this.tree, nextLinkedList, nextPriorKeys);
+                this.nodes.push(newNode);
+            }
+        } else {
+            for (var key of Object.keys(this.jsonData)) {
+                var currentData = this.jsonData[key];
+
+                //Construct references for the next Node
+                var nextLinkedList = [...this.linkedList];
+                var nextPriorKeys = [...this.priorKeys];
+                nextLinkedList.push(this);
+                nextPriorKeys.push(key);
+
+                var newNode = new Node(currentData, this.tree, nextLinkedList, nextPriorKeys);
+                this.nodes.push(newNode);
+            }
+        }
     }
 
     _getIterArr() {
